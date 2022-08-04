ad88b9b2118e30997b3805658dea0b9d0df3217c
ritshab <ritshab@littlehinges.com.au>
2022-08-04 22:47:03 +1000

    Setup structure

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(false);
+
+  const getResults = () => {
+    setLoading(true);
+    fetch('https://ihmmunealign-api.herokuapp.com/getihmmune/', {
+      method: "GET",
+      headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+      }
+    })
+    .then(res => { return res.json(); })
+    .then(response => {
+      setData(response)
+      setLoading(false);
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <button onClick={getResults}>Click</button>
+      {loading && <div>Loading...</div>}
+      {data && <div>{data.log_probability}</div>}
       </header>
     </div>
   );
