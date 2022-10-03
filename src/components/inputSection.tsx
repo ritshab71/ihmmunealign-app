2c77ad48550a35a7ff2e549483e96f8b0a4f9a20
ritshab <ritshab@littlehinges.com.au>
2022-10-03 14:56:34 +1100

    mvp-demo

--- src/components/inputSection.tsx
+++ src/components/inputSection.tsx
@@ -1,11 +1,20 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button'
 
-const InputSection = () => {
+type InputSectionProps = {
+    runIhmmuneAlign: (sequence: string) => void
+}
+
+const InputSection: FC<InputSectionProps> = (props: InputSectionProps) => {
+    const { runIhmmuneAlign } = props;
+    const [inputSequence, setInputSequence] = useState<string>('');
+
+    const handleRunIhmmuneAlign = () => {
+        runIhmmuneAlign(inputSequence);
+    }
 
     return (
-        <div className="flex flex-col basis-1/2 bg-white rounded p-4 space-y-3 text-sm uppercase font-medium text-slate-400 min-w-[400px]">
+        <div className="flex flex-col basis-2/5 bg-white rounded p-4 space-y-3 text-sm uppercase font-medium text-slate-400 min-w-[400px]">
             <div className="flex flex-col space-y-3 w-full basis-1/2">
                 <div className="w-full">IHMMUNE-ALIGN</div>
                 <div className="w-full p-2">
@@ -15,18 +24,25 @@ const InputSection = () => {
                         id="outlined-basic"
                         variant="outlined"
                         placeholder="Input your sequences"
-                        style={{fontSize: 11}}
                         rows={14}
+                        value={inputSequence || ''}
+                        onChange={(event) => setInputSequence(event.target.value)}
                     />
                 </div>
             </div>
             <div className="flex flex-col space-y-3 w-full basis-1/2">
                 <div className="w-full">Parameters</div>
                 <div className="w-full p-2">
+                    <div className="flex flex-col space-y-3 bg-slate-50 rounded p-3 h-full w-full text-xs text-slate-600 normal-case"></div>
                 </div>
             </div>
             <div className="flex justify-center items-center">
-                <button className="bg-blue-300 hover:bg-blue-500 text-white text-xs py-2 px-3 rounded">iHMMUNE-align</button>
+                <button
+                    className="bg-green-300 hover:bg-green-400 text-white text-xs py-2 px-3 rounded"
+                    onClick={handleRunIhmmuneAlign}
+                >
+                    iHMMUNE-align
+                </button>
             </div>
         </div>
     );
---
+++ src/components/resultsSection.tsx
@@ -0,0 +1,65 @@
+import React, { FC } from "react";
+import LineDivider from './common/lineDivider';
+import { IHMMuneResponseData } from "./dashboard";
+
+type ResultsSectionProps = {
+    ihmmuneAlignData: IHMMuneResponseData
+}
+
+const ResultsSection: FC<ResultsSectionProps> = (props: ResultsSectionProps) => {
+    const {ihmmuneAlignData} = props;
+
+    return (
+        <div className="flex flex-col basis-3/5 bg-white rounded p-4 space-y-3 text-sm uppercase font-medium text-slate-400 min-w-[400px]">
+            <LineDivider/>
+            <div className="flex flex-row w-full">
+                <div className="flex basis-2/3">A-score</div>
+                <div className="flex basis-1/3 justify-end items-center">
+                    <div className="bg-violet-300 text-white rounded px-2 py-1 h-8 w-full flex justify-center items-center text-xs">
+                        <span className="font-semibold">{ihmmuneAlignData.a_score}</span></div>
+                </div>
+            </div>
+            <LineDivider/>
+            <div className="flex flex-col space-y-2 basis-2/12 w-full">
+                <div className="w-full">V-gene</div>
+                <div className="flex flex-col space-y-3 bg-slate-50 rounded p-3 h-full w-full text-xs text-slate-600 normal-case">
+                    <div>Name: <span className="font-semibold">{ihmmuneAlignData.v_aln_info.v_seq_name}</span></div>
+                    <div className="flex flex-col space-y-1">
+                        <div>Sequence:</div>
+                        <span className="font-semibold break-words">{ihmmuneAlignData.v_aln_info.v_sequence}</span>
+                    </div>
+                </div>
+            </div>
+            <LineDivider/>
+            <div className="flex flex-col space-y-2 basis-2/12 w-full">
+                <div className="w-full">Alignment</div>
+                <div className="flex flex-col space-y-1 bg-slate-50 rounded p-3 h-full w-full text-xs text-slate-600 normal-case">
+                    <div>Score: <span className="font-semibold">{ihmmuneAlignData.v_aln_info.alignment_info.score}</span></div>
+                    <div>Matches: <span className="font-semibold">{ihmmuneAlignData.v_aln_info.alignment_info.matches}</span></div>
+                    <div>Gaps: <span className="font-semibold">{ihmmuneAlignData.v_aln_info.alignment_info.gaps}</span></div>
+                    <div>Mutations: <span className="font-semibold">{ihmmuneAlignData.v_aln_info.alignment_info.mutations}</span></div>
+                </div>
+            </div>
+            <LineDivider/>
+            <div className="flex flex-row w-full">
+                <div className="flex basis-2/3">State path probability</div>
+                <div className="flex basis-1/3 justify-end items-center">
+                    <div className="bg-blue-300 text-white rounded px-2 py-1 h-8 w-full flex justify-center items-center text-xs"><span className="font-semibold">{ihmmuneAlignData.log_probability}</span></div>
+                </div>
+            </div>
+            <LineDivider/>
+            <div className="flex flex-col space-y-2 basis-5/12 w-full overflow-y-auto">
+                <div className="w-full">HMM Info</div>
+                <div className="flex flex-col space-y-3 bg-slate-50 rounded p-3 h-full w-full text-xs text-slate-600 normal-case overflow-y-scroll">
+                    <div className="flex flex-col space-y-1">
+                        <div>State path:</div>
+                        <span className="font-semibold break-words text-[10px]">{ihmmuneAlignData.state_path.join(', ')}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
+}
+
+export default ResultsSection;
